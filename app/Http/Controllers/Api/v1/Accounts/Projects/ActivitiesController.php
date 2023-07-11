d4e5a2282739f2f3d52c85fa8b851f4e5eb75eb8
Bilal Rasool <478bilal@gmail.com>
2023-07-11 12:21:10 +0500

    added check to avoid addition of duplicate entries into Db of same activites

--- app/Http/Controllers/Api/v1/Accounts/Projects/ActivitiesController.php
+++ app/Http/Controllers/Api/v1/Accounts/Projects/ActivitiesController.php
@@ -10,6 +10,9 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Gate;
 use App\Http\Resources\ActivityResource;
 use App\Http\Requests\StoreProjectActivityRequest;
+use Carbon\Carbon;
+use Carbon\CarbonInterval;
+use Illuminate\Support\Facades\Cache;
 
 class ActivitiesController extends Controller
 {
@@ -19,14 +22,31 @@ class ActivitiesController extends Controller
             return api_response_unauthorized();
         }
 
-        $activity = $project->activities()->create($request->validated());
+        $from = CarbonInterval::seconds($request->from)->cascade()->format('%H:%I:%S');
+        $to = CarbonInterval::seconds($request->to)->cascade()->format('%H:%I:%S');
+    
+        $date = Carbon::parse($request->date); // Convert $request->date to a Carbon instance
+    
+        $start_datetime = Carbon::createFromFormat('Y-m-d H:i:s', $date->format('Y-m-d') . ' ' . $from)->toDateTimeString();
+        $end_datetime = Carbon::createFromFormat('Y-m-d H:i:s', $date->format('Y-m-d') . ' ' . $to)->toDateTimeString();
+    
+        
+        $last_activity_record = Activity::find(Cache::get('last_activity_id'));  
 
-        $this->storeScreenshots($request, $account, $activity);
+       
+        if(!($last_activity_record->start_datetime == $start_datetime && $last_activity_record->end_datetime == $end_datetime
+            && $last_activity_record->account_id == $account->id &&  $last_activity_record->project_id == $project->id))
+            {
+                $activity = $project->activities()->create($request->validated());
+                Cache::put('last_activity_id', $activity->id);
+                $this->storeScreenshots($request, $account, $activity);
 
-        return api_response([
-            'message' => 'The activity has been saved.',
-            'activity' => new ActivityResource($activity->refresh())
-        ], 200);
+                return api_response([
+                    'message' => 'The activity has been saved.',
+                    'activity' => new ActivityResource($activity->refresh())
+                ], 200);
+            }
+      
     }
 
     public function storeScreenshots(StoreProjectActivityRequest $request, Account $account, Activity $activity): void
